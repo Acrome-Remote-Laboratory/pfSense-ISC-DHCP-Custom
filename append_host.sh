69c8ce03a7002c470d3e32b46ac6cd4eb42ff2fd
Umut Ediz <umtedz@gmail.com>
2020-10-16 15:41:58 +0300

    FreeBSD does not like #!

--- append_host.sh
+++ append_host.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 cp ./dhcpd.opt82 /var/dhcpd/etc/dhcpd.opt82
 cp ./dhcpdoverride.sh /usr/local/etc/rc.d/dhcpdoverride.sh
 chmod +x /usr/local/etc/rc.d/dhcpdoverride.sh
--- dhcpdoverride.sh
+++ dhcpdoverride.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 killall -3 dhcpd
 cat /var/dhcpd/etc/dhcpd.opt82 >> /var/dhcpd/etc/dhcpd.conf
 /usr/local/sbin/dhcpd -user dhcpd -group _dhcp -chroot /var/dhcpd -cf /etc/dhcpd.conf -pf /var/run/dhcpd.pid em1 em2
