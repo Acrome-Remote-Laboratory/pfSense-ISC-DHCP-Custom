028a905f0a125ce2dcff99574965f2be4833abee
Umut Ediz <ediz-umut@windowslive.com>
2021-03-23 16:44:41 +0300

    Multiple switch support added

--- host-gen.py
+++ host-gen.py
@@ -1,31 +1,46 @@
-from pprint import pprint as print
-
 import os
 import pathlib
 
+class Switch:
+	def __init__(self, cid, subnet, port_count, ip_offset=0):
+		self.cid = cid
+		self.subnet = subnet
+		self._ip_prefix = '%d.0.%d.' % (self.subnet, self.cid)
+		self.port_count = port_count
+		self.forbidden_ports = []
+		self.ports = [port for port in range(1, port_count+1) if port not in self.forbidden_ports]
+		self.ip_offset = ip_offset
+
+	def disable_ports(self, ports):
+		self.forbidden_ports = ports
+		self.ports = [port for port in range(1, self.port_count+1) if port not in self.forbidden_ports]
+
+	def dump_host_definitions(self):
+		host_def = []
+		for port in self.ports:
+			ip = port + self.ip_offset
+			hostname = 'SW%dPORT%d' % (self.cid, port)
+			if ip < 255:
+				ip = self._ip_prefix + str(ip)
+				host_def.append("""
+				host %s  {
+					host-identifier option agent.circuit-id "%d.%d";
+					fixed-address %s;
+				}""" % (hostname, self.cid, port, ip))
+		return host_def
+
+SW1 = Switch(0, 172, 28, ip_offset=20)
+SW1.disable_ports([1,2,23,24,25,26,27,28])
+
+SW2 = Switch(1, 172, 18)
+SW2.disable_ports([15,17,18])
+
+host_definitions = SW1.dump_host_definitions() + SW2.dump_host_definitions()
+
 os.chdir(pathlib.Path(__file__).parent.absolute())
-
-def create_host_definition(switch_id, port_name, ip):
-	host_def = """
-	host port%d  {
-		host-identifier option agent.circuit-id "%d.%d";
-		fixed-address %s;
-	}""" % (port_name, switch_id, port_name, ip)
-	return host_def
-
-device_port_range = (3,22)
-total_port_count = 28
-ip_offset = 20
-ip_subnet = '172.0.0.'
-
-ip_addresses = [ip_subnet+"%d" % (i+1) for i in range(ip_offset, ip_offset + total_port_count)]
-port_ip_map = ["Port %2d -- IP: %s" % (i+1, ip_addresses[i]) for i in range(total_port_count)]
-host_definitions = [create_host_definition(0, i+1, ip_addresses[i]) for i in range(total_port_count)]
-
-
-with open('./dhcpd.opt82', 'w') as file:
-	file.write('')
+with open('./dhcpd.opt82', 'w+') as file:
+	for host in host_definitions:
+		file.write(host)
 
 with open('./dhcpd.opt82', 'a') as file:
-	for host in host_definitions[device_port_range[0]-1:device_port_range[1]]:
-		file.write(host)
+	pass
