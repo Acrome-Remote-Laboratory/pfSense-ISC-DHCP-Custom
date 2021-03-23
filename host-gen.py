1af10c84200ed6ebdad23a9e4e949a03316e64dc
Umut Ediz <ediz-umut@windowslive.com>
2021-03-23 23:44:39 +0300

    host list generation for nmap
    port config change

--- host-gen.py
+++ host-gen.py
@@ -1,39 +1,17 @@
 import os
 import pathlib
-
-class Switch:
-	def __init__(self, cid, subnet, port_count, ip_offset=0):
-		self.cid = cid
-		self.subnet = subnet
-		self._ip_prefix = '%s.%d.' % (self.subnet, self.cid)
-		self.port_count = port_count
-		self.forbidden_ports = []
-		self.ports = [port for port in range(1, port_count+1) if port not in self.forbidden_ports]
-		self.ip_offset = ip_offset
-
-	def disable_ports(self, ports):
-		self.forbidden_ports = ports
-		self.ports = [port for port in range(1, self.port_count+1) if port not in self.forbidden_ports]
-
-	def dump_host_definitions(self):
-		host_def = []
-		for port in self.ports:
-			ip = port + self.ip_offset
-			hostname = 'SW%dPORT%d' % (self.cid, port)
-			if ip < 255:
-				ip = self._ip_prefix + str(ip)
-				host_def.append("""
-				host %s  {
-					host-identifier option agent.circuit-id "%d.%d";
-					fixed-address %s;
-				}""" % (hostname, self.cid, port, ip))
-		return host_def
+from swconfig import Switch
 
 SW1 = Switch(0, '172.16', 28, ip_offset=20)
 SW1.disable_ports([1,2,23,24,25,26,27,28])
 
 SW2 = Switch(1, '172.16', 18)
-SW2.disable_ports([15,17,18])
+SW2.disable_ports([1,17,18])
+
+DHCP = Switch(0, '172.17', 256)
+
+Statics = Switch(0, '172.18', 2)
+Statics.disable_ports([1])
 
 host_definitions = SW1.dump_host_definitions() + SW2.dump_host_definitions()
 
@@ -42,5 +20,10 @@ with open('./dhcpd.opt82', 'w+') as file:
 	for host in host_definitions:
 		file.write(host)
 
-with open('./dhcpd.opt82', 'a') as file:
-	pass
+with open('./hosts', 'w+') as file:
+	for host in SW1.ip_list() + SW2.ip_list() + Statics.ip_list():
+		file.write(host + '\n')
+
+with open('./hosts-dhcp', 'w+') as file:
+	for host in SW1.ip_list() + SW2.ip_list() + DHCP.ip_list() + Statics.ip_list():
+		file.write(host + '\n')
---
+++ swconfig.py
@@ -0,0 +1,31 @@
+class Switch:
+	def __init__(self, cid, subnet, port_count, ip_offset=0):
+		self.cid = cid
+		self.subnet = subnet
+		self._ip_prefix = '%s.%d.' % (self.subnet, self.cid)
+		self.port_count = port_count
+		self.forbidden_ports = []
+		self.ip_offset = ip_offset
+
+		self.ports = [port for port in range(1, port_count+1) if port not in self.forbidden_ports]
+
+	def disable_ports(self, ports):
+		self.forbidden_ports = ports
+		self.ports = [port for port in range(1, self.port_count+1) if port not in self.forbidden_ports]
+
+	def dump_host_definitions(self):
+		host_def = []
+		for port in self.ports:
+			ip = port + self.ip_offset
+			hostname = 'SW%dPORT%d' % (self.cid, port)
+			if ip <= 255:
+				ip = self._ip_prefix + str(ip)
+				host_def.append("""
+				host %s  {
+					host-identifier option agent.circuit-id "%d.%d";
+					fixed-address %s;
+				}""" % (hostname, self.cid, port, ip))
+		return host_def
+
+	def ip_list(self):
+		return [self._ip_prefix + str(port + self.ip_offset) for port in self.ports if (port + self.ip_offset) <= 255]
