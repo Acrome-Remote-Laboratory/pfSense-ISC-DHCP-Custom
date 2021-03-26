01991e48a759e2696a3000db33bcade135789998
Umut Ediz <ediz-umut@windowslive.com>
2021-03-26 22:08:41 +0300

    TLV formatted cid match generation

--- swconfig.py
+++ swconfig.py
@@ -1,13 +1,13 @@
 _format_string = """ host %s  {
-	host-identifier option agent.circuit-id "%d.%d";
+	host-identifier option agent.circuit-id %s;
 	fixed-address %s;
 }"""
 
 class Switch:
-	def __init__(self, cid, subnet, port_count, ip_offset=0):
-		self.cid = cid
+	def __init__(self, rid, subnet, port_count, ip_offset=0):
+		self.rid = rid
 		self.subnet = subnet
-		self._ip_prefix = '%s.%d.' % (self.subnet, self.cid)
+		self._ip_prefix = '%s.%d.' % (self.subnet, self.rid)
 		self.port_count = port_count
 		self.forbidden_ports = []
 		self.ip_offset = ip_offset
@@ -22,10 +22,17 @@ class Switch:
 		host_def = []
 		for port in self.ports:
 			ip = port + self.ip_offset
-			hostname = 'SW%dPORT%d' % (self.cid, port)
+			hostname = 'SW%dPORT%d' % (self.rid, port)
 			if ip <= 255:
 				ip = self._ip_prefix + str(ip)
-				host_def.append( _format_string % (hostname, self.cid, port, ip))
+				t = 1 #Refer to TP-Link documentation about DHCP Option 82 TLV format
+				v = ("%d.%d" % (self.rid, port))
+				l = len(v)
+				v_iter = iter(v.encode('utf-8').hex())
+				#Inserts ':' after every 2nd character, works for hex representation
+				#since custom circuit-id is a string and valid characters are bigger than 9 in hex
+				cid = "%s:%s:%s" % (hex(t)[2:], hex(l)[2:], ':'.join(i+j for i,j in zip(v_iter, v_iter)))
+				host_def.append( _format_string % (hostname, cid, ip))
 		return host_def
 
 	def ip_list(self):
